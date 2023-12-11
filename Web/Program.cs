66c4978969274cbbebcbabf085173a55bd583e2c
MaximKotelok <98751837+MaximKotelok@users.noreply.github.com>
2023-12-11 21:51:01 +0200

    Swagger Added

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(
 		options => {
 			options.UseSqlServer(
@@ -15,12 +17,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 		);
 
 
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
