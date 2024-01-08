d034fa8663235e4d9e67927c987cd8b98d799ce5
MaximKotelok <98751837+MaximKotelok@users.noreply.github.com>
2024-01-08 15:01:59 +0200

    API: ProductAttributeGroup Controller for Get requests

---
+++ Services/AttributeGroupService/AttributeGroupService.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore.Query;
+using Repository.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Services.AttributeGroupService
+{
+    public class AttributeGroupService : IAttributeGroupService
+	{
+		private readonly IRepository<ProductAttributeGroup> _repository;
+
+		public AttributeGroupService(IRepository<ProductAttributeGroup> repository)
+		{
+			_repository = repository;
+		}
+
+		public void DeleteProductAttributeGroup(int id)
+		{
+			ProductAttributeGroup? group = _repository.Get(x => x.Id == id);
+			_repository.Remove(group);
+			_repository.SaveChanges();
+		}
+		public IEnumerable<ProductAttributeGroup> GetAllProductAttributeGroups(Expression<Func<ProductAttributeGroup, bool>>? filter = null, string? includeProperties = null)
+		{
+			return _repository.GetAll(filter, includeProperties);
+		}
+
+		public ProductAttributeGroup? GetProductAttributeGroup(Expression<Func<ProductAttributeGroup, bool>> filter, string? includeProperties = null)
+		{
+			return _repository.Get(filter, includeProperties);
+		}
+
+		
+	
+		public void InsertProductAttributeGroup(ProductAttributeGroup group)
+		{
+			_repository.Insert(group);
+		}
+
+		public void UpdateProductAttributeGroup(ProductAttributeGroup group)
+		{
+			_repository.Update(group);
+		}
+
+	}
+}
---
+++ Services/AttributeGroupService/IAttributeGroupService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.AttributeGroupService
+{
+	public interface IAttributeGroupService
+	{
+		IEnumerable<ProductAttributeGroup> GetAllProductAttributeGroups(Expression<Func<ProductAttributeGroup, bool>>? filter = null, string? includeProperties = null);
+		ProductAttributeGroup? GetProductAttributeGroup(Expression<Func<ProductAttributeGroup, bool>> filter, string? includeProperties = null);
+		void InsertProductAttributeGroup(ProductAttributeGroup attributeGroup);
+		void UpdateProductAttributeGroup(ProductAttributeGroup attributeGroup);
+		void DeleteProductAttributeGroup(int id);
+	}
+}
