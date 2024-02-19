bf57c7d3f27865f7da4ba7e444f3adef2fce774f
Artem Yurchenko <fatalyst361@gmail.com>
2024-02-20 00:14:58 +0200

    feat: add ProductFilterComponent

--- Web/ClientApp/src/components/Auth/AuthPageComponent.js
+++ Web/ClientApp/src/components/Auth/AuthPageComponent.js
@@ -6,73 +6,71 @@ import RegistrationForm from "./RegistrationFormComponent/RegistrationForm";
 import BtnSocialComponenent from "./components/BtnSocialComponenent/BtnSocialComponenent";
 import google from "../../assets/images/google.svg";
 import faceboo from "../../assets/images/Facebook.svg";
+import ProductFilterComponent from "../Common/ProductFilterComponent/ProductFilterComponent";
 const AuthPageComponent = () => {
-  const [activeForm, setActiveForm] = useState("login");
+    const [activeForm, setActiveForm] = useState("login");
 
-  const handleTabClick = (formType) => {
-    setActiveForm(formType);
-  };
-  return (
-    // <div className="input-group mb-3 center back-serach-bar">
-    <div
-      className="row"
-      style={{ justifyContent: "end", margin: "0 0 200px 0" }}
-    >
-      <div
-        className="col-12 col-md-6 col-lg-4 p-3"
-        style={{ marginTop: "50px" }}
-      >
-        <MenuComponent />
-      </div>
+    const handleTabClick = (formType) => {
+        setActiveForm(formType);
+    };
+    return (
+        // <div className="input-group mb-3 center back-serach-bar">
+        <div
 
-      <div className="col-12 col-md-6 col-lg-4 p-3">
-        <div className="mb-4">
-          <span
-            // href="#"
-            className={`me-3 ${styles["navigation-h"]} ${
-              activeForm === "login" ? styles["navigation-h-active"] : ""
-            }`}
-            onClick={() => handleTabClick("login")}
-          >
-            Вхід
-          </span>
-          <span
-            // href="#"
-            className={`${styles["navigation-h"]} ${
-              activeForm === "registration" ? styles["navigation-h-active"] : ""
-            }`}
-            onClick={() => handleTabClick("registration")}
-          >
-            Реєстрація
-          </span>
-        </div>
+            className="row"
+            style={{ justifyContent: "end", margin: "0 0 200px 0" }}
+        >
+            <div
+                className="col-12 col-md-6 col-lg-4 p-3"
+                style={{ marginTop: "50px" }}
+            >
+                <MenuComponent />
+            </div>
 
-        <div className="form-container"> 
-          {activeForm === "login" && <LoginForm />}
-          {activeForm === "registration" && <RegistrationForm />}
+            <div className="col-12 col-md-6 col-lg-4 p-3">
+                <div className="mb-4">
+                    <span
+                        // href="#"
+                        className={`me-3 ${styles["navigation-h"]} ${activeForm === "login" ? styles["navigation-h-active"] : ""
+                            }`}
+                        onClick={() => handleTabClick("login")}
+                    >
+                        Вхід
+                    </span>
+                    <span
+                        // href="#"
+                        className={`${styles["navigation-h"]} ${activeForm === "registration" ? styles["navigation-h-active"] : ""
+                            }`}
+                        onClick={() => handleTabClick("registration")}
+                    >
+                        Реєстрація
+                    </span>
+                </div>
 
-          
-        </div>
-      </div>
+                <div className="form-container">
+                    {activeForm === "login" && <LoginForm />}
+                    {activeForm === "registration" && <RegistrationForm />}
+                </div>
+            </div>
 
-      <div className="col-12 col-md-6 col-lg-4 p-3">
-        <div className="mb-3">
-          <span  className={`${styles["login-social-text"]} `}>
-            Увійти через:
-          </span>
+            <div className="col-12 col-md-6 col-lg-4 p-3">
+                <div className="mb-3">
+                    <span className={`${styles["login-social-text"]} `}>
+                        Увійти через:
+                    </span>
+                </div>
+                <div className="mb-3">
+                    <BtnSocialComponenent icon={google} text={"Вхід через Google"} />
+                    <BtnSocialComponenent icon={faceboo} text={"Вхід через Facebook"} />
+                </div>
+                <div>
+                    <a href="#" className={`${styles["login-ugota-text"]} `}>
+                        Угода про конфіденційність:
+                    </a>
+                </div>
+            </div>
         </div>
-        <div className="mb-3">
-          <BtnSocialComponenent icon={google} text={"Вхід через Google"} />
-          <BtnSocialComponenent icon={faceboo} text={"Вхід через Facebook"} />
-        </div>
-        <div>
-          <a href="#" className={`${styles["login-ugota-text"]} `}>
-            Угода про конфіденційність:
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AuthPageComponent;
---
+++ Web/ClientApp/src/components/Common/ProductFilterComponent/ProductFilterComponent.js
@@ -0,0 +1,57 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const ProductFilterComponent = ({ products }) => {
+    const [filters, setFilters] = useState({});
+    const [availableAttributes, setAvailableAttributes] = useState({});
+
+    useEffect(() => {
+        const attributes = {};
+        products.forEach(product => {
+            product.Properties.forEach(property => {
+                const { Attribute, Value } = property;
+                if (Attribute && Value) {
+                    const attributeName = Attribute.Name || '';
+                    const attributeValue = Value;
+                    if (!attributes[attributeName]) {
+                        attributes[attributeName] = [];
+                    }
+                    if (!attributes[attributeName].includes(attributeValue)) {
+                        attributes[attributeName].push(attributeValue);
+                    }
+                }
+            });
+        });
+        setAvailableAttributes(attributes); // Оновлюємо стан availableAttributes
+        setFilters({});
+    }, [products]);
+
+    const handleFilterChange = (attributeName, selectedValue) => {
+        setFilters(prevFilters => ({
+            ...prevFilters,
+            [attributeName]: selectedValue,
+        }));
+    };
+
+    return (
+        <div>
+            {Object.keys(filters).map(attributeName => (
+                <div key={attributeName}>
+                    <span>{attributeName}:</span>
+                    <select
+                        value={filters[attributeName] || ''}
+                        onChange={e => handleFilterChange(attributeName, e.target.value || null)}
+                    >
+                        <option value="">All</option>
+                        {availableAttributes[attributeName]?.map(value => (
+                            <option key={value} value={value}>
+                                {value}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProductFilterComponent;
