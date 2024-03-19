db87f8c50644e71c0fa4530c9c283094bf3e7b1c
Dmytro <dima1510059@gmail.com>
2024-03-20 00:05:34 +0200

    Edit Details styles WEB

--- Web/ClientApp/src/components/Common/CustomImgComponent/CustomImgComponent.js
+++ Web/ClientApp/src/components/Common/CustomImgComponent/CustomImgComponent.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CustomImgComponent = ({ src, alt, defaultSrc, className, style }) => {
-  const [imageSrc, setImageSrc] = useState((src?src:defaultSrc));
+  const [imageSrc, setImageSrc] = useState(src ? src : defaultSrc);
 
   const handleImageError = () => {
     setImageSrc(defaultSrc);
   };
 
   return (
-    <img    
-      className={className} 
+    <img
+      className={className}
       style={style}
       src={imageSrc}
       alt={alt}
-      onError={handleImageError}      
+      onError={handleImageError}
     />
   );
 };
--- Web/ClientApp/src/components/User/Pages/Details/Component/HeadOfDetailsComponent/HeadOfDetailsComponent.js
+++ Web/ClientApp/src/components/User/Pages/Details/Component/HeadOfDetailsComponent/HeadOfDetailsComponent.js
@@ -1,55 +1,81 @@
 import React, { useEffect, useState } from "react";
-import styles from "./HeadOfDetailsComponent.module.css"
+import styles from "./HeadOfDetailsComponent.module.css";
 import { NavigationDetailsComponent } from "../../../../Common/NavigationDetailsComponent/NavigationDetailsComponent";
 import CustomImgComponent from "../../../../../Common/CustomImgComponent/CustomImgComponent";
-
+import { BtnRadioSelectedProduct } from "../../../../../Common/BtnRadioSelectedProductComponent/BtnRadioSelectedProduct";
+import { AdditionalDivRadio } from "../AdditionalComponent/AdditionalDivRadio";
+import { ReactComponent as Heart } from "../../../../../../assets/images/details/heart.svg";
 // import setupAccordion from "./AccordionSideMenuJQ";
 // import $ from "jquery";
 
-
 const HeadOfDetailsComponent = ({ product }) => {
   return (
-    <div className='d-flex w-100 mb-5'>
+    <div className="d-flex w-100 mb-5">
+      <div style={{ position: "relative" }}>
+        <Heart className={`position-absolute top-0  ${styles["heart-styles"]} `}  />
+        <CustomImgComponent
+          className="me-2"
+          style={{ width: "568px", height: "400px", objectFit: "cover" }}
+          defaultSrc="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
+          src={`${product.pathToPhoto}`}
+        />
+      </div>
+      <div className="w-75">
+        <NavigationDetailsComponent id={product.id} />
 
-      <CustomImgComponent
-      className="me-2"
-        style={{ width: "568px", height: "400px", objectFit: "cover" }}
-        defaultSrc="https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
-        src={`${product.pathToPhoto}`}
-      />
-      <div className='w-75'>
-        <NavigationDetailsComponent id={product.id}/>
-
-        <div className={styles['product-details-info-container']}>
+        <div className={styles["product-details-info-container"]}>
           <div>
-            <p className={styles['product-details-title']}>{product.title + " " + product.shortDescription}</p>
-            <p className={styles['product-details-in-pharmacies']}>в 406 аптеках</p>
+            <p className={styles["product-details-title"]}>
+              {product.title + " " + product.shortDescription}
+            </p>
+            <p className={styles["product-details-in-pharmacies"]}>
+              в 406 аптеках
+            </p>
             <hr />
           </div>
           <div>
-            <p className={styles["price-in-your-city"]}>Ціни у <b>{product.city}</b></p>
-            <p className={styles["product-price-range"]}>від
-              <span className={styles["product-details-price"]}> {Number(product.from).toFixed(2)} </span>
+            <p className={styles["price-in-your-city"]}>
+              Ціни у <b className={styles["in-your-city"]}>{product.city}</b>
+            </p>
+            <p className={styles["product-price-range"]}>
+              від
+              <span className={styles["product-details-price"]}>
+                {" "}
+                {Number(product.from).toFixed(2)}{" "}
+              </span>
               до
-              <span className={styles["product-details-price"]}> {Number(product.to).toFixed(2)} </span>
+              <span className={styles["product-details-price"]}>
+                {" "}
+                {Number(product.to).toFixed(2)}{" "}
+              </span>
               грн
             </p>
           </div>
+
+          <div>
+            <AdditionalDivRadio title="Смак" />
+          </div>
+
           <hr />
 
-          <div className='row d-flex justify-content-between'>
-            <div className='col-6'>
-              <a className={`btn ${styles["btn-details"]} ${styles["btn-pharmacies"]} w-100`} href={`/map/${product.id}`}>
+          <div className="row d-flex justify-content-between">
+            <div className="col-6">
+              <a
+                className={`btn ${styles["btn-details"]} ${styles["btn-pharmacies"]} w-100`}
+                href={`/map/${product.id}`}
+              >
                 Знайти в аптеках
               </a>
             </div>
-            <div className='col-6'>
-              <a className={`btn ${styles["btn-details"]} ${styles["btn-analogues"]} w-100`} href={`/map}`}>
+            <div className="col-6">
+              <a
+                className={`btn ${styles["btn-details"]} ${styles["btn-analogues"]} w-100`}
+                href={`/map}`}
+              >
                 Аналоги
               </a>
             </div>
           </div>
-
         </div>
       </div>
     </div>
